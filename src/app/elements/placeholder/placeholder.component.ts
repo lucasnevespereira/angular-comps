5577529b7009e425370a608a7461379a15840abc
lucas.nevespereira <lucas.nevespereira@eemi.com>
2020-05-21 12:18:39 +0200

    conditional displaying placeholder header

--- src/app/elements/placeholder/placeholder.component.ts
+++ src/app/elements/placeholder/placeholder.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-placeholder',
-  templateUrl: './placeholder.component.html',
-  styleUrls: ['./placeholder.component.css']
+  selector: "app-placeholder",
+  templateUrl: "./placeholder.component.html",
+  styleUrls: ["./placeholder.component.css"],
 })
 export class PlaceholderComponent implements OnInit {
+  @Input() header: boolean = true;
+  @Input() lines: number = 3;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
