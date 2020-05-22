26420226c56a5bfec5250f49b74024c6a833b7cb
lucas.nevespereira <lucas.nevespereira@eemi.com>
2020-05-22 16:48:30 +0200

    table component created with some data

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-collections-home',
-  templateUrl: './collections-home.component.html',
-  styleUrls: ['./collections-home.component.css']
+  selector: "app-collections-home",
+  templateUrl: "./collections-home.component.html",
+  styleUrls: ["./collections-home.component.css"],
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: "James", age: 24, job: "Designer" },
+    { name: "Phillip", age: 26, job: "Engineer" },
+    { name: "Elyse", age: 25, job: "Engineer" },
+  ];
 
-  constructor() { }
+  headers = [
+    { key: "name", label: "Name" },
+    { key: "age", label: "Age" },
+    { key: "job", label: "Job" },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
