61db4abbb9495a98e90ac18c99a229809dfad61c
lucas.nevespereira <lucas.nevespereira@eemi.com>
2020-05-25 15:30:49 +0200

    communicating stats data to child component

--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-views-home',
-  templateUrl: './views-home.component.html',
-  styleUrls: ['./views-home.component.css']
+  selector: "app-views-home",
+  templateUrl: "./views-home.component.html",
+  styleUrls: ["./views-home.component.css"],
 })
 export class ViewsHomeComponent implements OnInit {
+  stats = [
+    { value: 22, label: "Users" },
+    { value: 9, label: "Revenue" },
+    { value: 500, label: "Reviews" },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
